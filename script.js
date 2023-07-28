c50e95515a8503b5f56a68b9d36e38b09e0645ca
korduss <bernatkordian23@gmail.com>
2023-07-28 17:53:15 +0200

    create function for single round with if-statements

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const types = ["rock", "paper", "scissors"]
 function getComputerChoice(){
     return types[Math.floor(Math.random() * 3)];
 }
-console.log(getComputerChoice());
-function singleRound(playerSelection, computerSelection){
 
+function singleRound(playerSelection, computerSelection){
+    console.log(computerSelection);
+    console.log(playerSelection);
+    if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "Great Job! Rock beat scissors";
+    }else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "Great Job! Paper beat rock";
+    }else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "Great Job! Scissors beat paper";
+    }else if(playerSelection == "rock" && computerSelection == "paper"){
+        return "You Lose! Paper beat rock";
+    }else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "You Lose! Scissors beat paper";
+    }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return "You Lose! Rock beat scissors";
+    }else{
+        return "Tie!!";
+    }
 }
+console.log(singleRound(playerSelection, getComputerChoice()))
