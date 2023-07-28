65d36b30dcc27f34294e21a1f7dc74678de67c5d
korduss <bernatkordian23@gmail.com>
2023-07-28 17:40:36 +0200

    Create player selection with prompt

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const playerSelection = prompt("Rock, Paper or Scissors?");
+console.log(playerSelection);
 const types = ["rock", "paper", "scissors"]
 function getComputerChoice(){
     return types[Math.floor(Math.random() * 3)];
 }
 console.log(getComputerChoice());
+function singleRound(playerSelection, computerSelection){
+
+}
